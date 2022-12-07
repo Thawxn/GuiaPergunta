6ede779510e9b9c3808d481089a770c6710ed459
Thawan Marques <111232361+Thawxn@users.noreply.github.com>
2022-12-07 17:57:01 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sequelize = require("sequelize");
 
-const connection = new sequelize('guiaperguntas', 'root', '******.', {
+const connection = new sequelize('guiaperguntas', 'root', '2001', {
     host: 'localhost',
     dialect: 'mysql'
 });
